49b4341d6659da81ce505e916d40387895724fe0
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-09-21 14:40:46 -0400

    edit group-sizes.js

--- group-sizes.js
+++ group-sizes.js
@@ -4,7 +4,6 @@ const num = Number(prompt("Please Enter A Number: "))
 var group3 = 0
 var group2 = 0
 var temp = 0
-var temp2 = 0
 
 if(num % 3 === 0) {
     group3 = num / 3
@@ -18,9 +17,9 @@ if(num % 3 != 0) {
     }
     else if(temp % 2 != 0) {
         group3 = Math.trunc((num / 3) - 1)
-        temp2 = group3 * 3
-        temp2 = num - temp2
-        group2 = temp2 / 2
+        temp = group3 * 3
+        temp = num - temp
+        group2 = temp / 2
     }
     console.log(`You will have ${group3} group(s) of 3 and ${group2} group(s) of 2`)
 }
